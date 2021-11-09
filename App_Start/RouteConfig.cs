d1398ef1a086f7f41d2f705b426679a13e02844d
CELINA RINALDI <44110736@comunidadort.edu.ar>
2021-11-09 17:25:55 -0300

    Agregar archivos de proyecto.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebProducts
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index"}
+            );
+
+            routes.MapRoute(
+                name: "IndexByCategory",
+                url: "{controller}/{action}/{category}/{name}",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "Index",
+                    category = UrlParameter.Optional,
+                    name = UrlParameter.Optional
+                }
+
+                );
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProducts.Data;
+using WebProducts.Models;
+
+namespace WebProducts.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDbContext context = new ProductDbContext();
+        // GET: Product
+
+        /*  public ActionResult Index(string category = "")
+          {
+              if(string.IsNullOrEmpty(category))
+              {
+                  return View(context.Products.ToList());
+              }
+              else
+              {
+                  return View((from p in context.Products
+                               where p.Category == category
+                               select p).ToList<Product>());
+              }
+          } */
+
+        public ActionResult Index(string category, string name)
+        {
+            int cat = String.IsNullOrEmpty(category) ? 0 : 2;
+            int nam = String.IsNullOrEmpty(name) ? 0 : 1;
+            int val = cat | nam;
+         switch(val)
+            {
+                case 0:
+                    return View(context.Products.ToList());
+                case 2:
+                    return View((from p in context.Products
+                                 where p.Category == category
+                                 select p).ToList<Product>());
+                case 1:
+                    return View((from p in context.Products
+                                 where p.ProductName == name
+                                 select p).ToList<Product>());
+
+                case 3:
+                    return View((from p in context.Products
+                                 where p.Category == category &&  p.ProductName == name select p).ToList<Product>());
+
+                default:
+                    return View(); 
+            }
+        }
+
+
+
+        public ActionResult Detail(int id)
+        {
+            Product producto = context.Products.Find(id);
+            if(producto != null)
+            {
+                return View("Detail", producto);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Product producto = context.Products.Find(id);
+            if (producto != null)
+            {
+                return View("Edit", producto);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult EditConfirmed(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(product).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Edit", product);
+        }
+
+        [HttpGet] 
+        public ActionResult Create()
+        {
+            Product producto = new Product();
+            return View("Create", producto);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product producto)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(producto);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Create", producto);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Product product = context.Products.Find(id);
+            if (product != null)
+            {
+                return View("Delete", product);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = context.Products.Find(id);
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
